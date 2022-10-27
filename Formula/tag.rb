f318bb8ee9ace4c927a4adf7c04addab0dbd0d95
Chris Snead <105823816+sneadc@users.noreply.github.com>
2022-10-27 04:01:08 -0400

    version bump

--- Formula/tag.rb
+++ Formula/tag.rb
@@ -1,8 +1,8 @@
 class Tag < Formula
   desc "Manipulate and query tags on macOS files"
   homepage "https://github.com/sneadc/tag/"
-  url "https://github.com/sneadc/tag/archive/refs/tags/v2022.4.03.tar.gz"
-  sha256 "3dc73c5391b97ee4eac1bec57f84ec1957ef4aa8ba5b85eabb1d1bcdf97e1f0e"
+  url "https://github.com/sneadc/tag/archive/refs/tags/v2022.4.04.tar.gz"
+  sha256 "a0a57356767eebbad0023e285b7317e08269270e095cc850bc57f852dc0d8c60"
   license "MIT"
   revision 1
   head "https://github.com/sneadc/tag.git", branch: "master"
