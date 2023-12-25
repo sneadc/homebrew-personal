b6be2ab49f0f3676a7b35a61935a0d6f0b93c978
Chris Snead <105823816+sneadc@users.noreply.github.com>
2023-12-25 09:54:04 -0500

    checksum added

---
+++ Formula/checksum.rb
@@ -0,0 +1,24 @@
+class Tag < Formula
+  desc "Manipulate and query tags on macOS files"
+  homepage "https://github.com/sneadc/checksum/"
+  url "https://github.com/sneadc/checksum/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "fdc10448de2976e001905d7c7b3166a07821245fc03e50dc4c9777938b7bbf25"
+  license "MIT"
+  revision 1
+  head "https://github.com/sneadc/checksum.git", branch: "main"
+
+  depends_on :macos
+
+  def install
+    system "pip", "install", "-e", "."
+    generate_completions_from_executable(bin/"checksum", "completions")
+  end
+
+  test do
+    test_checksum = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+    test_file = Pathname.pwd+"test_file"
+    touch test_file
+    # system "#{bin}/checksum", "-a", "256", test_file
+    assert_equal test_checksum, `#{bin}/checksum #{test_file}`.chomp
+  end
+end
